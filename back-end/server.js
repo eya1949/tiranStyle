94ee86484d5b3bd46bb0bff19e50330641f175b4
eya1949 <ayabcr.dev@gmail.com>
2026-05-21 10:37:36 +0100

    auth feature

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,22 @@
-import express from "express"
-import cors from "cors";
-import connectDB from "./src/config/db.js";
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
-// Connect to MongoDB
 
-connectDB();
+app.use("/api/auth", authRoutes);
 
-app.get("/",(req,res)=>{
-    res.json({message:"Server is running"})
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-
-
-app.listen(PORT, ()=>{
-  console.log(`🚀 Server listening on http://localhost:${PORT}`);
-    
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
+});
